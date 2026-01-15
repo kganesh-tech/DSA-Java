c90ab38d1523dae920f5ff3e28480ac7eae87d75
kganesh-tech <k.ganesh6286@gmail.com>
2026-01-15 11:43:33 +0530

    Add files via upload

---
+++ day-3-Sliding Window-Hashing/Longest Substring Without Repeating Characters.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class LongestSubstringUnique {
+
+    public static int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(c);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s)); 
+    }
+}
